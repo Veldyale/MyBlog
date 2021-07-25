a2d4af1c3c4b75492cdc1327275c65eacc02cebe
veldyale <veldyale@icloud.com>
2021-07-25 18:46:00 +0400

    create postresql bd

--- home_app/models.py
+++ home_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Home(models.Model):
+    home_image = models.ImageField(upload_to='home_images/')
+    home_text = models.CharField(max_length=300)
+
