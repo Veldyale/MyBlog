bada4d688e53048f0322920f9376f694835adc14
veldyale <veldyale@icloud.com>
2021-07-25 22:58:49 +0400

    Add post model

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home_app.apps.HomeAppConfig',
-    'blog_app',
+    'blog_app.apps.BlogAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
