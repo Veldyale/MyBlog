82dd26ef8d5f4f9c352da953dcb6cc05c716b2a4
veldyale <veldyale@icloud.com>
2021-07-28 21:41:19 +0400

    add bootstrap

--- blog_app/models.py
+++ blog_app/models.py
@@ -4,5 +4,6 @@ class Post(models.Model):
     title = models.CharField(max_length=300)
     date = models.DateTimeField()
     text = models.TextField()
-    image = models.ImageField(upload_to='blog_images/')
+    image = models.FileField(upload_to='blog_images/')
+    # file = models.FileField(upload_to='pdf_files/')
 
